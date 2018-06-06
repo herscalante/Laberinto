0ccbd5d851044ec6037ed211295af0502a058078
J29599 <30739942+J29599@users.noreply.github.com>
2018-06-06 16:38:59 -0300

    Add files via upload

---
+++ LaberintoGeneradorMDI/src/arbol/Arbol.java
@@ -0,0 +1,37 @@
+package arbol;
+/**
+ *
+ * @author Jeremias Reyes, Leandro Navarrete, Hernan Escalante
+ */
+
+
+//Clase del arbol
+public class Arbol {
+    private NodoArbol raiz;
+    private NodoArbol hojaFin;
+    
+    public Arbol(){
+        this.raiz=null;
+        this.hojaFin=null;
+    }
+    public Arbol(NodoArbol raiz) {
+        this.raiz = raiz;
+    }
+
+    public NodoArbol getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(NodoArbol raiz) {
+        this.raiz = raiz;
+    }
+
+    public void setFin(NodoArbol fin) {
+        this.hojaFin = fin;
+    }
+    
+    public NodoArbol getHojaFin() {
+        return hojaFin;
+    }
+    
+}
---
+++ LaberintoGeneradorMDI/src/arbol/NodoArbol.java
@@ -0,0 +1,74 @@
+package arbol;
+
+/**
+ *
+ * @author Jeremias Reyes, Leandro Navarrete, Hernan Escalante
+ */
+
+//Clase encargada del manejo de los nodos de los arboles
+public class NodoArbol {
+    private boolean visitado;
+    private NodoArbol enlaceArriba;
+    private NodoArbol enlaceAbajo;
+    private NodoArbol enlaceIzquierdo;
+    private NodoArbol enlaceDerecho;
+    private NodoArbol anterior;
+    
+    public NodoArbol(){
+        visitado=false;
+        enlaceArriba=null;
+        enlaceAbajo=null;
+        enlaceIzquierdo=null;
+        enlaceDerecho=null;
+        anterior=null;
+    }
+
+    public void setEnlaceAbajo(NodoArbol enlaceAbajo) {
+        this.enlaceAbajo = enlaceAbajo;
+    }
+
+    public void setEnlaceArriba(NodoArbol enlaceArriba) {
+        this.enlaceArriba = enlaceArriba;
+    }
+
+    public void setEnlaceDerecho(NodoArbol enlaceDerecho) {
+        this.enlaceDerecho = enlaceDerecho;
+    }
+
+    public void setEnlaceIzquierdo(NodoArbol enlaceIzquierdo) {
+        this.enlaceIzquierdo = enlaceIzquierdo;
+    }
+
+    public NodoArbol getEnlaceAbajo() {
+        return enlaceAbajo;
+    }
+
+    public NodoArbol getEnlaceArriba() {
+        return enlaceArriba;
+    }
+
+    public NodoArbol getEnlaceDerecho() {
+        return enlaceDerecho;
+    }
+
+    public NodoArbol getEnlaceIzquierdo() {
+        return enlaceIzquierdo;
+    }
+    
+    public void setVisitado(boolean visitado) {
+        this.visitado = visitado;
+    }
+
+    public NodoArbol getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(NodoArbol anterior) {
+        this.anterior = anterior;
+    }
+    
+    public boolean isVisitado() {
+        return visitado;
+    }
+    
+}
